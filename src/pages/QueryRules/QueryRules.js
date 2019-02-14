244e4411259817a1ea0d3db9ab5b37bdcfcc295f
jyash97 <jyash97@gmail.com>
2019-02-14 17:49:41 +0530

    Add Promoted Results

---
+++ src/pages/QueryRules/QueryRules.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { string, func, bool } from 'prop-types';
+import { connect } from 'react-redux';
+import get from 'lodash/get';
+import URL from 'url-parser-lite';
+
+import {
+	setCurrentApp,
+	getPermission as getPermissionFromAppbase,
+} from '../../batteries/modules/actions';
+
+import Loader from '../../components/Loader';
+import { getURL } from '../../constants/config';
+
+class QueryRules extends Component {
+	componentDidMount() {
+		const { credentials } = this.props;
+		if (!credentials) {
+			this.init();
+		}
+	}
+
+	componentDidUpdate(prevProps) {
+		const { appName } = this.props;
+		if (appName !== prevProps.appName) {
+			this.init();
+		}
+	}
+
+	init() {
+		// prettier-ignore
+		const {
+			updateCurrentApp,
+			appName,
+			// getPermission,
+		} = this.props;
+		updateCurrentApp(appName);
+		// getPermission(appName);
+	}
+
+	render() {
+		const { appName, credentials, isCluster } = this.props;
+		const SCALR_API = getURL();
+		const { protocol, host } = URL(SCALR_API);
+		const url = `${protocol}://${credentials}@${host}`;
+		const iframeURL = `https://arc-promoted-results.netlify.com/promoted-results-queries/?appname=${
+			isCluster ? '*' : appName
+		}&url=${url}&footer=false&sidebar=false&appswitcher=false&mode=edit&cloneApp=false&oldBanner=false`;
+
+		return (
+			<section>
+				{credentials ? (
+					<iframe
+						height={`${window.innerHeight - 60 || 600}px`}
+						width="100%"
+						title="dejavu"
+						src={iframeURL}
+						frameBorder="0"
+					/>
+				) : (
+					<Loader />
+				)}
+			</section>
+		);
+	}
+}
+
+QueryRules.defaultProps = {
+	isCluster: false,
+};
+
+QueryRules.propTypes = {
+	appName: string.isRequired,
+	credentials: string.isRequired,
+	updateCurrentApp: func.isRequired,
+	isCluster: bool,
+};
+
+const mapStateToProps = (state) => {
+	const { username, password } = get(state, 'user.data', {});
+	return {
+		credentials: username ? `${username}:${password}` : '',
+	};
+};
+
+const mapDispatchToProps = dispatch => ({
+	updateCurrentApp: appName => dispatch(setCurrentApp(appName, appName)),
+	getPermission: appName => dispatch(getPermissionFromAppbase(appName)),
+});
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps,
+)(QueryRules);
