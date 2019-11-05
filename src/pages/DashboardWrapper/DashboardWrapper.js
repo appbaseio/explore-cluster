d08c20470e1fb6489e1a97f9a459c126bd79cdc8
bietkul <kuldepsaxena155@gmail.com>
2019-11-05 19:45:05 +0530

    fix: minor fixes

--- src/pages/DashboardWrapper/DashboardWrapper.js
+++ src/pages/DashboardWrapper/DashboardWrapper.js
@@ -40,7 +40,7 @@ const defaultRoutes = {
 		icon: 'dashboard',
 		menu: [
 			{ label: 'Search Templates', link: '/cluster/search-templates', tag: 'Beta' },
-			{ label: 'Query Suggestions', link: 'query-suggestions', tag: 'Beta' },
+			{ label: 'Query Suggestions', link: '/cluster/query-suggestions', tag: 'Beta' },
 		],
 	},
 	'Browse Data': {
