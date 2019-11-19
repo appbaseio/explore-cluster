aee9a5396698ef055ad6605f2db9566c66d126e3
Siddharth Kothari <sids.aquarius@gmail.com>
2019-11-19 16:18:01 +0530

    Update utils.js

--- src/components/PricingTable/utils.js
+++ src/components/PricingTable/utils.js
@@ -3,15 +3,15 @@ import { ARC_PLANS } from '../../batteries/utils';
 // eslint-disable-next-line
 export const MESSAGES = {
 	[ARC_PLANS.ARC_BASIC]:
-		'Are you sure to downgrade your current plan to Arc basic? Please press Ok to continue.',
+		'Arc Basic plan works with a single ElasticSearch node cluster. Press Ok to continue.',
 	[ARC_PLANS.ARC_STANDARD]:
-		'Arc standard plan works with 3 elasticsearch nodes. Please press Ok to continue.',
+		'Arc Standard plan works with up to 3 ElasticSearch nodes. Press Ok to continue.',
 	[ARC_PLANS.ARC_ENTERPRISE]:
-		'Get the top features like custom events and query suggestions with Arc enterprise plan. Please press Ok to continue.',
+		'Arc Enterprise plan works with up to 10 ElasticSearch nodes and comes with additional features such as custom analytics and query suggestions. Press Ok to continue upgrading.',
 	[ARC_PLANS.HOSTED_ARC_BASIC]:
-		'Are you sure to downgrade your current plan to Arc basic? Please press Ok to continue.',
+		'Arc Basic plan works with a single ElasticSearch node cluster. Press Ok to continue.',
 	[ARC_PLANS.HOSTED_ARC_STANDARD]:
-		'Arc standard plan works with 3 elasticsearch nodes. Please press Ok to continue.',
+		'Arc Standard plan works with up to 3 ElasticSearch nodes. Press Ok to continue.',
 	[ARC_PLANS.HOSTED_ARC_ENTERPRISE]:
-		'Get the top features like custom events and query suggestions with Arc enterprise plan. Please press Ok to continue.',
+		'Arc Enterprise plan works with up to 10 ElasticSearch nodes and comes with additional features such as custom analytics and query suggestions. Press Ok to continue upgrading.',
 };
