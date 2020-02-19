af311a6885167507613aece54ec8598e098d6265
anuj <anujshah584@gmail.com>
2020-02-19 12:20:26 +0530

    refactor: invoke function

---
+++ src/components/InvokeFunctionModal/InvokeResponse.js
@@ -0,0 +1,26 @@
+import { Row } from 'antd';
+import React from 'react';
+import { modalHeading } from '../../pages/HomePage/styles';
+
+// eslint-disable-next-line import/prefer-default-export
+export function InvokeResponse({ responseData, status }) {
+	console.log('response data', responseData);
+	const { headers, ...rest } = responseData;
+	// ___headers___['X-Duration-Seconds']
+	return (
+		<>
+			<Row>
+				<h3 className={modalHeading}>Response Status</h3>
+				{status}
+			</Row>
+			<Row>
+				<h3 className={modalHeading}>Execution Time</h3>
+				{headers['X-Duration-Seconds']}s
+			</Row>
+			<Row>
+				<h3 className={modalHeading}>Response Data</h3>
+				<pre>{JSON.stringify(rest, null, 4)}</pre>
+			</Row>
+		</>
+	);
+}
