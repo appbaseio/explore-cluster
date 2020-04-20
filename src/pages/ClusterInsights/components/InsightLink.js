2d6f39eb343e168467403c4dc3c17a154b9adb1f
jyash97 <jyash97@gmail.com>
2020-04-20 14:37:39 +0530

    use Frame Component

--- src/pages/ClusterInsights/components/InsightLink.js
+++ src/pages/ClusterInsights/components/InsightLink.js
@@ -1,35 +1,59 @@
 import React from 'react';
-import { Result, Button } from 'antd';
+import { Result, Button, Spin } from 'antd';
+import Frame from '../../../components/Frame';
 
-const InsightLink = ({ hasSubscribed, insight_link }) => {
-	if (!hasSubscribed) {
-		return 'Placeholder Image';
+class InsightLink extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			frameLoading: !!props.insight_link,
+		};
 	}
 
-	if (hasSubscribed && !insight_link) {
+	toggleLoader = () => {
+		this.setState(state => ({
+			frameLoading: !state.frameLoading,
+		}));
+	};
+
+	render() {
+		const { hasSubscribed, insight_link } = this.props;
+		const { frameLoading } = this.state;
+
+		if (!hasSubscribed) {
+			return 'Placeholder Image';
+		}
+
+		if (hasSubscribed && !insight_link) {
+			return (
+				<Result
+					status="404"
+					title="Insights not available"
+					subTitle="Analysis is in progress, insights will be available soon. Contact us for more details."
+					extra={
+						<Button onClick={() => window.Intercom('show')} type="primary">
+							Chat with us
+						</Button>
+					}
+				/>
+			);
+		}
+
 		return (
-			<Result
-				status="404"
-				title="Insights not available"
-				subTitle="Analysis is in progress, insights will be available soon. Contact us for more details."
-				extra={
-					<Button onClick={() => window.Intercom('show')} type="primary">
-						Chat with us
-					</Button>
-				}
-			/>
+			<React.Fragment>
+				{frameLoading && <Spin />}
+				<Frame
+					src={insight_link}
+					id="curated-insights"
+					onLoad={this.toggleLoader}
+					height="600px"
+					width="100%"
+					title="Curated Insights"
+					frameBorder="0"
+				/>
+			</React.Fragment>
 		);
 	}
-
-	return (
-		<iframe
-			src={insight_link}
-			height="600px"
-			width="100%"
-			title="Curated Insights"
-			frameBorder="0"
-		/>
-	);
-};
+}
 
 export default InsightLink;
