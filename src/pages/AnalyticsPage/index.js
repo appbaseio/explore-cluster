c5f8186a535a072fa5660528028ea65442e5cb7c
jyash97 <jyash97@gmail.com>
2020-05-18 20:42:40 +0530

    fix: remove replay search from search preview

--- src/pages/AnalyticsPage/index.js
+++ src/pages/AnalyticsPage/index.js
@@ -39,7 +39,7 @@ const AnalyticsView = ({ appName, isPaidUser, plan }) => (
 				<Container>
 					<Analytics
 						filterId="analytics_page"
-						displayReplaySearch
+						displayReplaySearch={window.location.pathname.startsWith('/app')}
 						chartWidth={window.innerWidth - 400}
 						appName={appName}
 					/>
