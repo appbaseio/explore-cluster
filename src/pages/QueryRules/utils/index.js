5a851651e387b0185e77f70a54708ecf5130c69a
Siddharth Kothari <sids.aquarius@gmail.com>
2020-06-15 22:04:32 +0530

    content: update rules tooltip content

--- src/pages/QueryRules/utils/index.js
+++ src/pages/QueryRules/utils/index.js
@@ -115,7 +115,7 @@ const bannerDetails = {
 const toolTips = {
 	promote_result: (
 		<>
-			Promote a result and show it at a specific position within the search results.{' '}
+			Promote a document and show it at a specific position within the search results.{' '}
 			<a
 				target="_blank"
 				rel="noopener noreferrer"
@@ -127,7 +127,7 @@ const toolTips = {
 	),
 	hide_result: (
 		<>
-			Hide a document from search results.{' '}
+			Hide document(s) from search results.{' '}
 			<a
 				target="_blank"
 				rel="noopener noreferrer"
@@ -139,7 +139,7 @@ const toolTips = {
 	),
 	replace_search_term: (
 		<>
-			Replace the whole search term with another search term.{' '}
+			Replace an end-user provided search term with a specified search term.{' '}
 			<a
 				target="_blank"
 				rel="noopener noreferrer"
@@ -151,7 +151,7 @@ const toolTips = {
 	),
 	custom_data: (
 		<>
-			Add extra JSON data to be returned with your search results.{' '}
+			Add extra JSON data to be returned with the search results.{' '}
 			<a
 				target="_blank"
 				rel="noopener noreferrer"
@@ -163,7 +163,7 @@ const toolTips = {
 	),
 	function: (
 		<>
-			Add a custom function to make changed without any limitations.{' '}
+			Add a user-defined function to pre or post-process when trigger conditions are met.{' '}
 			<a
 				target="_blank"
 				rel="noopener noreferrer"
@@ -175,7 +175,7 @@ const toolTips = {
 	),
 	remove_words: (
 		<>
-			Remove a word(s) from the search term.{' '}
+			Remove specified word(s) from the end-user provided search term.{' '}
 			<a
 				target="_blank"
 				rel="noopener noreferrer"
@@ -187,7 +187,7 @@ const toolTips = {
 	),
 	replace_words: (
 		<>
-			Replace all the instances of a word in the applied search term.{' '}
+			Replace all the instances of the specified word(s) in the end-user provided search term.{' '}
 			<a
 				target="_blank"
 				rel="noopener noreferrer"
@@ -199,8 +199,7 @@ const toolTips = {
 	),
 	add_filter: (
 		<>
-			Allows you to define the term filters that will get applied on the search type of
-			queries.{' '}
+			Add one or more term queries (aka filters) to the incoming search query.{' '}
 			<a
 				target="_blank"
 				rel="noopener noreferrer"
@@ -232,7 +231,7 @@ const toolTips = {
 	),
 	search_settings: (
 		<>
-			Define the dataField and fieldWeights for your search type of queries.{' '}
+			Set data field(s) and field weight(s) to be applied to the search query.{' '}
 			<a
 				target="_blank"
 				rel="noopener noreferrer"
