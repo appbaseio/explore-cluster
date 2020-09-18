c9d1abb787cf8bb142a9eaab47872c96d102226b
Nikhil Varma <nikhilvarma892@gmail.com>
2020-09-18 19:18:38 +0530

    chore: Upgrade Sentry version from 4 to 5

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -31,9 +31,7 @@ class ErrorPage extends React.Component {
 			error: true,
 		});
 		Sentry.withScope((scope) => {
-			Object.keys(errorInfo).forEach((key) => {
-				scope.setExtra(key, errorInfo[key]);
-			});
+			scope.setExtras(errorInfo);
 			Sentry.captureException(error);
 		});
 	}
