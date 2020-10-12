3158cca34335e5e95353c4636a2964f9ad340d50
bietkul <kuldepsaxena155@gmail.com>
2020-10-12 17:48:43 +0530

    feat: add feature exception for ecommerce

--- src/pages/IntegrationsPage/index.js
+++ src/pages/IntegrationsPage/index.js
@@ -2,40 +2,35 @@ import React from 'react';
 import get from 'lodash/get';
 import { connect } from 'react-redux';
 import { bool, string } from 'prop-types';
-// import { isValidPlan } from '../../batteries/utils';
-// import Overlay from '../../components/Overlay';
-// import Banner from '../../batteries/components/shared/UpgradePlan/Banner';
+import { isValidPlan } from '../../batteries/utils';
+import Overlay from '../../components/Overlay';
+import Banner from '../../batteries/components/shared/UpgradePlan/Banner';
 import Main from './Main';
-// TODO: Update details
-// TODO: Update docs links
-// const bannerDetails = {
-// 	title: 'Integrations',
-// 	description: 'GUI to manage preferences for appbase.io integrations.',
-// 	buttonText: 'Read more',
-// 	icon: 'pencil',
-// 	href: 'https://docs.appbase.io/docs/integrations/',
-// };
+
+const bannerDetails = {
+	title: 'Integrations',
+	description: 'GUI to manage preferences for appbase.io integrations.',
+	icon: 'pencil',
+};
 
 const IntegrationsPage = ({ tier, featureEcommerce }) => {
-	// TODO: Enable plan validation after BE changes with update image
-	// if (!isValidPlan(tier, featureEcommerce)) {
-	// 	return (
-	// 		<React.Fragment>
-	// 			<Banner {...bannerDetails} />
-	// 			<Overlay
-	// 				style={{
-	// 					maxWidth: '70%',
-	// 				}}
-	// 				// TODO: Update image
-	// 				src="https://i.imgur.com/c6P8eN8.png"
-	// 				alt="integrations"
-	// 			/>
-	// 		</React.Fragment>
-	// 	);
-	// }
+	if (!isValidPlan(tier, featureEcommerce)) {
+		return (
+			<React.Fragment>
+				<Banner {...bannerDetails} />
+				<Overlay
+					style={{
+						maxWidth: '70%',
+					}}
+					src="https://i.imgur.com/ziZMrZm.png"
+					alt="integrations"
+				/>
+			</React.Fragment>
+		);
+	}
 	return (
 		<React.Fragment>
-			{/* <Banner {...bannerDetails} /> */}
+			<Banner {...bannerDetails} />
 			<Main />
 		</React.Fragment>
 	);
