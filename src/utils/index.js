f0425400aa08c9f849839a0e02a94f22cca37666
Lakhan Samani <lakhan.m.samani@gmail.com>
2020-11-07 18:46:10 +0530

    fix: field weight decimals

--- src/utils/index.js
+++ src/utils/index.js
@@ -576,10 +576,10 @@ export function getSubFields({
 				};
 			}, {});
 
-		return { [address]: weight, ...subFields };
+		return { [address]: weight.toFixed(1), ...subFields };
 	}
 
-	return { [address]: weight };
+	return { [address]: weight.toFixed(1) };
 }
 
 function ltrim(str) {
