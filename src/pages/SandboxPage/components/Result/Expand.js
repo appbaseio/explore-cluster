f0fc2bd317de079159e8398c58307db0a0fb2bd8
Lakhan Samani <lakhan.m.samani@gmail.com>
2020-11-23 22:28:09 +0530

    fix: add type check for children

--- src/pages/SandboxPage/components/Result/Expand.js
+++ src/pages/SandboxPage/components/Result/Expand.js
@@ -29,13 +29,16 @@ class Expand extends React.Component {
 	render() {
 		const { className, children } = this.props;
 		const { hasOverflow, collapsed } = this.state;
+
 		return (
 			<React.Fragment>
 				<div
 					ref={this.currentRef}
 					className={`${className} ${hasOverflow && collapsed ? 'collapse' : ''}`}
 				>
-					{children({ hasOverflow, collapsed })}
+					{typeof children === 'function'
+						? children({ hasOverflow, collapsed })
+						: children}
 				</div>
 				{hasOverflow ? (
 					<div className="expand-button-container">
