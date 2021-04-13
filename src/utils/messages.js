4ad3ae09fe9f1d1c0c1e4d2e701a5a99e8712842
bietkul <kuldepsaxena155@gmail.com>
2021-04-13 19:03:14 +0530

    fix: update messages

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -74,7 +74,7 @@ export const suggestionsMessages = {
 
 export const cacheMessages = {
 	max_duration: Message(
-		'The time to live for a cached request in seconds. The default value is 360s.',
+		'The time to live for a cached request in seconds. The default value is 300s.',
 	),
 	max_size: Message(
 		'The memory limit for the cached requests in MB(s). The default value is 128MB.',
