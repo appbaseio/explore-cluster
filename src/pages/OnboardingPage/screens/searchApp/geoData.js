aa1928dc16940912b7239354521d7f814fc3c6e1
lohi212 <suvvarilohita@gmail.com>
2021-10-29 15:28:30 +0530

    fix:CSB for geo

--- src/pages/OnboardingPage/screens/searchApp/geoData.js
+++ src/pages/OnboardingPage/screens/searchApp/geoData.js
@@ -14,6 +14,7 @@ import {
 import {
 	ReactiveGoogleMap,
   } from "@appbaseio/reactivemaps";
+  import { notification } from 'antd';
 import appbaseHelpers from '../../utils/appbaseHelpers';
 import { putSettings, getSettings } from '../../../../batteries/modules/actions';
 import { getURL } from '../../../../constants/config';
