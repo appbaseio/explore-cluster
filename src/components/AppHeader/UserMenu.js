e473348cbabc4f3193c73a8ab72680323cf92c01
Shubham Shah <shubhamshahrising@gmail.com>
2022-11-15 17:09:32 +0530

    feat: upgrade drop-down prop

--- src/components/AppHeader/UserMenu.js
+++ src/components/AppHeader/UserMenu.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { object } from 'prop-types';
 import get from 'lodash/get';
 import { DownOutlined, PoweroffOutlined, UserOutlined } from '@ant-design/icons';
-import { Menu, Avatar, Dropdown } from 'antd';
+import { Avatar, Dropdown } from 'antd';
 import { css } from 'react-emotion';
 import { media } from '../../utils/media';
 
@@ -35,17 +35,20 @@ const handleLogout = () => {
 	window.location.href = '/';
 };
 
-const menu = (
-	<Menu>
-		<Menu.Item onClick={handleLogout} data-cy="logout-button">
-			<PoweroffOutlined />
-			Logout
-		</Menu.Item>
-	</Menu>
-);
+const menu = [
+	{
+		key: '1',
+		label: (
+			<div onClick={handleLogout} data-cy="logout-button">
+				<PoweroffOutlined />
+				Logout
+			</div>
+		),
+	},
+];
 
 const UserMenu = ({ user }) => (
-	<Dropdown overlay={menu} className={userMenu} trigger={['click']}>
+	<Dropdown menu={{ items: menu }} className={userMenu} trigger={['click']}>
 		<div style={{ cursor: 'pointer' }} data-cy="logout-menu">
 			<Avatar icon={<UserOutlined />} />
 			&nbsp;&nbsp;
