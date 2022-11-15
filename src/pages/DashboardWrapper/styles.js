22bffe38689e1de7863333615ed0ab99fbda6507
Shubham Shah <shubhamshahrising@gmail.com>
2022-11-15 12:02:10 +0530

    fix: styles for searchbox

--- src/pages/DashboardWrapper/styles.js
+++ src/pages/DashboardWrapper/styles.js
@@ -3,12 +3,9 @@ import { css } from 'emotion';
 const searchInputStyle = css`
 	text-align: center;
 	padding: 10px 16px;
-	input {
-		opacity: 0.3;
-
-		&:focus {
-			opacity: 1;
-		}
+	opacity: 0.3;
+	&:focus-within {
+		opacity: 1;
 	}
 `;
 
