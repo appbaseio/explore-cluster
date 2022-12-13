c940e73a20ea11f0d33aa720d9b4c8f75bab8f3f
lohi212 <suvvarilohita@gmail.com>
2022-12-13 18:00:45 +0530

    fix: no iframe with searchWrapeper

--- src/pages/IntegrationsPage/shared/PreviewModal/StoreFrontPreview.js
+++ src/pages/IntegrationsPage/shared/PreviewModal/StoreFrontPreview.js
@@ -201,19 +201,23 @@ class StoreFrontPreview extends React.Component {
 						onChange={this.handleProductSelection}
 					/>
 				)}
-				<iframe
-					title="iframe-preview"
-					id="iframe-preview"
-					ref={this.iframeRef}
-					frameBorder="0"
-					style={{
-						border: isMobile ? '1px solid rgb(204, 204, 204)' : undefined,
-					}}
-					width={isMobile ? 400 : '100%'}
-					height={window.innerHeight - 60}
-					src="about:blank"
-					onLoad={this.onLoad}
-				/>
+				{!displayProductPicker || currentProduct ? (
+					<iframe
+						title="iframe-preview"
+						id="iframe-preview"
+						ref={this.iframeRef}
+						frameBorder="0"
+						style={{
+							border: isMobile ? '1px solid rgb(204, 204, 204)' : undefined,
+						}}
+						width={isMobile ? 400 : '100%'}
+						height={window.innerHeight - 60}
+						src="about:blank"
+						onLoad={this.onLoad}
+					/>
+				) : (
+					<></>
+				)}
 			</div>
 		);
 	}
