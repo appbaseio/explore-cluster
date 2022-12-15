55b78fda838f02ba544b0edd6a11c742872cf1c6
Shubham Shah <shubhamshahrising@gmail.com>
2022-12-15 11:45:52 +0530

    fix: review change

--- src/pages/ConfigureBackendPage/ConfigureBackend.js
+++ src/pages/ConfigureBackendPage/ConfigureBackend.js
@@ -352,7 +352,9 @@ const ConfigureBackend = (props) => {
 											>
 												<Card className={`field-label `} bordered={false}>
 													<Typography.Text>
-														<Typography.Text danger>*</Typography.Text>
+														<Typography.Text type="danger">
+															*
+														</Typography.Text>
 														&nbsp; Choose search engine{' '}
 													</Typography.Text>
 												</Card>
